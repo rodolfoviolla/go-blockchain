a4e075ba44386fb0125b56e10d39d0acecb73b1f
Rodolfo Violla <rodolfo.violla@gmail.com>
2023-02-14 15:48:09 -0300

    chore: separates block and blockchain logic

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,9 +1,4 @@
 package blockchain
-
-type BlockChain struct {
-	Blocks []*Block
-}
-
 type Block struct {
 	Hash []byte
 	Data []byte
@@ -20,16 +15,6 @@ func CreateBlock(data string, prevHash []byte) *Block {
 	return block
 }
 
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.Blocks = append(chain.Blocks, new)
-}
-
 func Genesis() *Block {
 	return CreateBlock("Genesis", []byte{})
-}
-
-func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
 }
